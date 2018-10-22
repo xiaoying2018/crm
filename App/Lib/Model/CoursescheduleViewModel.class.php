7161c4764c118d695160760a64c944916a06bf3b
jasper <240023810@qq.com>
2018-10-22 15:19:33 +0800

    添加 课程回放 可按照 上课时间进行排序

--- App/Lib/Model/CoursescheduleViewModel.class.php
+++ App/Lib/Model/CoursescheduleViewModel.class.php
@@ -1,8 +1,8 @@
 <?php
 class CoursescheduleViewModel extends ViewModel{
     public $viewFields = array(
-        'Schedule'=>array('id'=>'sc_id','serial','_table'=>"education.course_schedule"),
-        'Section'=>array('id'=>'s_id','name'=>'section_name','_table'=>"education.course_section",'_on'=>'Schedule.section_id = Section.id','_type'=>'LEFT'),
+        'Schedule'=>array('id'=>'sc_id','serial','start_time','_table'=>"education.course_schedule"),
+        'Section'=>array('id'=>'s_id','name'=>'section_name','duration','_table'=>"education.course_section",'_on'=>'Schedule.section_id = Section.id','_type'=>'LEFT'),
         'Period'=>array('id'=>'p_id','name'=>'period_name','_table'=>"education.course_period",'_on'=>'Period.id = Schedule.period_id','_type'=>'LEFT'),
         'Course'=>array('id'=>'c_id','name'=>'course_name','_table'=>"education.course",'_on'=>'Course.id = Period.course_id','_type'=>'LEFT'),
 
